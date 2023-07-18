093e73ec5e6043251087149bd3a7f2fbe01928ac
nickmoura <nickmoura26@gmail.com>
2023-07-18 16:21:06 -0300

    javascript incrementado

--- JavaScript/ex001/script.js
+++ JavaScript/ex001/script.js
@@ -1,1 +1,3 @@
 window.alert('Minha primeira mensagem!')
+window.confirm('Está gostando de JS?') // Janela com botão "Ok" e cancelar
+window.prompt('Qual é seu nome?') // Vai perguntar o nome do usuário

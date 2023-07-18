02b99b9a469c4c66e7475b44ffb3d1e7e8b0111a
nickmoura <nickmoura26@gmail.com>
2023-07-18 18:22:08 -0300

    Criação da pasta de Python

---
+++ Python/Scripts - Python/flask/tinta.py
@@ -0,0 +1,7 @@
+l = float(input('Qual a largura da parede? '))
+a = float(input('Qual a altura da parede? '))
+ar = l*a
+t = ar/2
+print('Sua parede tem uma dimensão de {} x {}, tendo uma área de {} m².'.format(l, a, ar))
+def result():
+    print('Você precisará de {:.0f} litros de tinta para pintar esta parede.'.format(t))
